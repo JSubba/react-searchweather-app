70cf071ace2b1e95dbc014761aa1792df0c2a48b
JSubba <prolearn1000@protonmail.com>
2021-11-07 10:26:10 +0900

    change commit

--- src/components/weatherSearch.js
+++ src/components/weatherSearch.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
-export const URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "d66e30fdec8d12402d3e283b9c0260e9";
+export const URL = `https://api.openweathermap.org/data/2.5/weather`;
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const weatherSearch = async (query) => {
-    const { data } = await axios.get(URL, {
-        params: {
-            q: query,
-            units: "metric",
-            APPID: API_KEY,
-        },
-    });
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
 
-    return data;
+  console.log(data);
+
+  return data;
 };
