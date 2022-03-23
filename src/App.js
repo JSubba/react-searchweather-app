932a29a04a75092819d9b63d0b894a49ae75622b
JSubba <prolearn1000@protonmail.com>
2022-03-23 17:32:41 +0900

    changes made

--- src/App.js
+++ src/App.js
@@ -4,26 +4,26 @@ import { weatherSearch } from "./components/weatherSearch";
 import WeatherContainer from "./components/WeatherContainer";
 
 const App = () => {
-    const [query, setQuery] = useState("");
-    const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-    const search = async (e) => {
-        if (e.key === "Enter") {
-            const data = await weatherSearch(query);
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await weatherSearch(query);
 
-            setWeather(data);
-            setQuery("");
-        }
-    };
+      setWeather(data);
+      setQuery("");
+    }
+  };
 
-    return (
-        <WeatherContainer
-            query={query}
-            setQuery={setQuery}
-            search={search}
-            weather={weather}
-        />
-    );
+  return (
+    <WeatherContainer
+      query={query}
+      setQuery={setQuery}
+      search={search}
+      weather={weather}
+    />
+  );
 };
 
 export default App;
