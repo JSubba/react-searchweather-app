974647c1d04984c65be1c53b629d554a097dc562
JSubba <prolearn1000@protonmail.com>
2022-05-20 18:34:48 +0900

    added build folder

---
+++ build/precache-manifest.794a9763cbe846edff2f50b731343768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02f180f2db594d149acd",
+    "url": "/react-searchweather-app/static/css/main.ca4b39db.chunk.css"
+  },
+  {
+    "revision": "02f180f2db594d149acd",
+    "url": "/react-searchweather-app/static/js/main.02f180f2.chunk.js"
+  },
+  {
+    "revision": "be3eadc62dea6223b445",
+    "url": "/react-searchweather-app/static/js/1.be3eadc6.chunk.js"
+  },
+  {
+    "revision": "0d76789efa8208153e7d",
+    "url": "/react-searchweather-app/static/js/runtime~main.0d76789e.js"
+  },
+  {
+    "revision": "3defb4c954c4142c9f2ec13eaf52fe67",
+    "url": "/react-searchweather-app/static/media/city-bg.3defb4c9.jpg"
+  },
+  {
+    "revision": "6c70ba17463e993ab4d58aa73a42b839",
+    "url": "/react-searchweather-app/index.html"
+  }
+];
